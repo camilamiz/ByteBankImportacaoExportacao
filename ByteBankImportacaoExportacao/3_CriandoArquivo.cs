42e9b6140aa75e7009ac2b4b2fd4f0e399884ddc
camilamiz <camila.yuri.m@gmail.com>
2021-08-30 17:36:14 -0300

    Using flush method for StreamWriter.

--- ByteBankImportacaoExportacao/3_CriandoArquivo.cs
+++ ByteBankImportacaoExportacao/3_CriandoArquivo.cs
@@ -37,5 +37,29 @@ namespace ByteBankImportacaoExportacao
                 escritor.Write("456,65456,4556.0,Pedro");
             }
         }
+
+        static void TestaEscrita()
+        {
+            var caminhoArquivo = "teste.txt";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoArquivo, FileMode.Create))
+            using (var escritor = new StreamWriter(fluxoDeArquivo))
+            {
+                for (int i = 0; i < 1000000000; i++)
+                {
+                    //não é nesse momento que a linha é escrita no arquivo. se abrirmos o arquivo no meio do loop, não vamos ver a linha acima
+                    //para fazer o código acima, usamos a ram, que é muito rápida, no entanto, para escrever no arquivo no hd, demora muito mais tempo
+                    //o buffer armazena as informações para que o sistema seja chamado menos vezes
+                    //no entanto, tem ocasiões em que não quero usar o buffer, quero ser notificado de imediato, caso algum problema ocorra
+                    //para isso usamos o Flush - ele despeja o buffer para o stream!
+                    escritor.WriteLine($"linha {i}");
+                    escritor.Flush(); 
+
+                    Console.WriteLine($"linha {i} foi escrita no arquivo. Tecle enter para adicionar mais uma.");
+                    Console.ReadLine();
+                }
+
+            }
+        }
     }
 }
--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -14,7 +14,8 @@ namespace ByteBankImportacaoExportacao
         static void Main(string[] args)
         {
             //CriarArquivo();
-            CriarArquivoComWriter();
+            //CriarArquivoComWriter();
+            TestaEscrita();
 
             Console.ReadLine();
         }
